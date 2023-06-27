838b8a67a7d4b9e6a00727cebec72d202bbd1a16
Braganholo <braganhologuilherme@gmail.com>
2023-06-27 18:23:51 -0300

    add status to purchase and sale

--- src/app/models/sale.js
+++ src/app/models/sale.js
@@ -8,6 +8,7 @@ class Sale extends Model {
         product: Sequelize.INTEGER,
         price: Sequelize.FLOAT,
         quantity: Sequelize.INTEGER,
+        status: Sequelize.INTEGER,
       },
       {
         sequelize,
@@ -21,6 +22,7 @@ class Sale extends Model {
   static associate(models) {
     this.belongsTo(models.Product, {foreignKey: 'product'}),
     this.belongsTo(models.Buyer, {foreignKey: 'buyer'})
+    this.belongsTo(models.Buyer, {foreignKey: 'status'})
   }
 }
 
